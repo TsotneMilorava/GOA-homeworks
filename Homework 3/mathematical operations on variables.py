c2be3c6725f4ee38d0eeb4a41ce2800ee0c5dd00
TsotneMilorava <tsotnemilorava166@gmail.com>
2024-10-19 17:53:14 +0400

    Add files via upload

---
+++ Homework 3/mathematical operations on variables.py
@@ -0,0 +1,17 @@
+#რაღაც მნიშვნელობას რისდამი მოქმედებას ვახორციელებთ შეგვიძლია აღვნიშნოთ ცვლადით.
+#ავღნიშნოთ x=5 
+x=5
+#ავღნიშნოთ y=x+5
+y=x+5
+print(x+y)
+print(x/y)
+print(x//y)
+print(x-y)
+print(x*y)
+print(x*x+y*y+2*x*y)
+x="ts"
+y="otnes"
+print(x+y)
+z=" uyvars "
+d= " ieso "
+print(x+y+z+d)
